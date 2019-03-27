aec38e67351a3a37b81435cfdbbacbea1a167550
haf001 <46520973+haf001@users.noreply.github.com>
2019-03-27 13:54:51 +0100

    Updated code for Darcy multi-compartment porous media
    Updated the variational form to consider the transfer of fluid out of one compartment and also into the next compartment as previous variational equation did not cater to this conservation of mass transfer

--- Darcy_Flow_Multicompartment.py
+++ Darcy_Flow_Multicompartment.py
@@ -2,7 +2,6 @@ from fenics import *
 import numpy as np
 from ufl import nabla_div
 import sympy as sym
-import matplotlib.pyplot as plt
 
 def solver(f, b_12_val, b_23_val, mesh, degree=2):
     """
@@ -39,7 +38,6 @@ def solver(f, b_12_val, b_23_val, mesh, degree=2):
     # Defining variational problem
     (p_1, p_2, p_3) = TrialFunctions(W)
     (v_1, v_2, v_3) = TestFunctions(W)
-
     f_1, f_2, f_3 = f
     d = 2
     I = Identity(d)
@@ -49,7 +47,9 @@ def solver(f, b_12_val, b_23_val, mesh, degree=2):
     K_3 = M*I
     b_12 = Constant(b_12_val)
     b_23 = Constant(b_23_val)
-    a = inner(K_1*grad(p_1), grad(v_1))*dx + inner(K_2*grad(p_2), grad(v_2))*dx + inner(K_3*grad(p_3), grad(v_3))*dx + inner(b_12*(p_1 - p_2), v_1)*dx + inner(b_23*(p_2 - p_3), v_2)*dx
+    a = inner(K_1*grad(p_1), grad(v_1))*dx + inner(K_2*grad(p_2), grad(v_2))*dx + inner(K_3*grad(p_3), grad(v_3))*dx + inner(b_12*(p_1 - p_2), v_1)*dx - inner(b_12*(p_1 - p_2), v_2)*dx + inner(b_23*(p_2 - p_3), v_2)*dx
+    - inner(b_23*(p_2 - p_3), v_3)*dx
+
     L = inner(f_1, v_1)*dx + inner(f_2, v_2)*dx + inner(f_3, v_3)*dx
 
     # Computing Numerical Pressure
@@ -61,7 +61,10 @@ def solver(f, b_12_val, b_23_val, mesh, degree=2):
 def run_solver():
     "Run solver to compute and post-process solution"
 
-    mesh = UnitSquareMesh(40, 40)
+    mesh = UnitSquareMesh(50, 50)
+    Velm = FiniteElement('P', mesh.ufl_cell(), degree=1)
+    Welm = MixedElement([Velm, Velm, Velm])
+    W = FunctionSpace(mesh, Welm)
 
     # Setting up problem parameters and calling solver
     d = 2
@@ -74,27 +77,16 @@ def run_solver():
     b_23_val = 0.03
     b_12 = Constant(b_12_val)
     b_23 = Constant(b_23_val)
-    
-    p_a = Expression('1 - x[0]*x[0]', degree=2)
-    p_b = Expression('1 - x[0]*x[0]', degree=2)
-    p_c = Expression('1 - x[0]*x[0]', degree=2)
-    
-    p_1 = interpolate(p_a, FunctionSpace(mesh, 'P', 2))
-    p_2 = interpolate(p_b, FunctionSpace(mesh, 'P', 2))
-    p_3 = interpolate(p_c, FunctionSpace(mesh, 'P', 2))
 
-    f_1 = Expression(('-2*x[0]', '0.0'), degree=1, domain=mesh)
-    f_2 = Expression(('-2*x[0]', '0.0'), degree=1, domain=mesh)
-    f_3 = Expression(('-2*x[0]', '0.0'), degree=1, domain=mesh)
-
-    f1 = -nabla_div(dot(K_1, f_1)) + b_12*(p_1 - p_2)
-    f2 = -nabla_div(dot(K_2, f_2)) + b_23*(p_2 - p_3)
-    f3 = -nabla_div(dot(K_3, f_3))
+    f1 = Constant(0.0)
+    f2 = Constant(0.0)
+    f3 = Constant(0.0)
 
     p = solver((f1, f2, f3), b_12_val, b_23_val, mesh, 2)
     # p here is a coefficient vector of mixed function space. To get components
 
     p_1, p_2, p_3 = p.split(deepcopy=True)
+
     u_bar1 = -K_1*grad(p_1)
     u_bar2 = -K_2*grad(p_2)
     u_bar3 = -K_3*grad(p_3)
